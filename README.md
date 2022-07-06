59b9ac2500358a22ee1920eda5f2e8c258ae118b
DESKTOP-KTATCQS\javi <javipena1@hotmail.com>
2022-07-07 01:17:47 +0200

    .gitignore funciona correctamente

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # twitterBot
+Bot de twitter encargado mediante tweepy publicado de tuits.
--- main.py
+++ main.py
@@ -1,23 +1,65 @@
-import re
-import pprint
+import datetime
+import logging as logger
+import time 
+import os.path
+import tweepy
+import schedule
+import credentials
+from tweepy.auth import OAuthHandler
 
 class App:
-    def __init__(self, apikey,apikey_secret, bearer_token):
+    def __init__(self):
         """
         Creates a new instance with the API key
         :param apikey:
         """
-        self.apikey = apikey
-        self.apikey_secret = apikey_secret
-        self.bearer_token = bearer_token
+        #self.auth = tweepy.AppAuthHandler(credentials.API_key, credentials.API_secret_key)
+        #self.auth.set_access_token(credentials.access_token,credentials.access_token_secret)
+        self.auth = OAuthHandler(credentials.API_key, credentials.API_secret_key)
+        self.auth.set_access_token(credentials.access_token, credentials.access_token_secret)
+        self.api = tweepy.API(self.auth)
+        
+    def logger(self, logtext):
+        logger.debug(str(datetime.datetime.now()) + " | " + logtext)
+        print(str(datetime.datetime.now()) + " | " + logtext)
 
-
+    def job(self):
+        self.logger("Estado")
+        #Definiremos la variable como global
+        global lastTweet_ID
+        lastTweet_ID = 0
+        # Leer lineas del archivo
+        file = open("tweets.txt", "r")
+        tweets = file.readlines()
+        self.logger(str(tweets))
+        self.logger("ID:" + str(lastTweet_ID) + "   Texto: " + tweets[lastTweet_ID])
+        # Publicar estado
+        self.api.update_status(tweets[lastTweet_ID])
+        # Actualizar variable ID con la del nuevo tweet
+        lastTweet_ID = lastTweet_ID + 1;
+        # Comprobar que no hemos sobrepasado el limite de publicaciones
+        if lastTweet_ID >= len(tweets):
+            lastTweet_ID = 0
+        self.logger("Updated status.")
+        # Abrir config y guardar ID de este tweet
+        file = open("config.txt", "w+")
+        file.writelines(str(lastTweet_ID))
+        file.close()
 
     def start(self):
-     """funcion utilizada para lanzar test"""
-        print("funcion")
-        
-
+        """funcion utilizada para lanzar test"""
+        try:
+            self.api.auth
+            self.logger("Authentication Successful")
+        except:
+            self.logger("Authentication Error")
+        self.job()
+        # Programar cada 60 segundos
+        schedule.every(60).seconds.do(self.job)
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+                
 
     def __str__(self):
         """
@@ -29,8 +71,6 @@ class App:
 
 if __name__ == "__main__":
     # Runtime around 3/4 seconds per match
-    app = App('SspqTw5jspgRdH2KLk3GmEMLn',
-                'RsImEASvEx21sLD09lNoQAiyNVQtKxWu8gzdCV6qGvkGubt6I0',
-                'AAAAAAAAAAAAAAAAAAAAAGEkegEAAAAAUTtrkarPpc6w0sPxGBpjCbYzMU0%3DR8Ma8gS9xDqeDaZ9BO05oqNh6aiCahZP8lS9MX2OI8CqsviC8E')
+    app = App()
     print(app)
    # print(app.end_time - app.start_time)
